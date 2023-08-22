37e52677f3ae95f897d1f79d28b4289371026a2a
StanParaclete <you@example.com>
2023-08-22 07:17:08 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/index';
+import styles from './App.module.css';
+
+import image from './images/image.png';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: {},
+      country: '',
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
